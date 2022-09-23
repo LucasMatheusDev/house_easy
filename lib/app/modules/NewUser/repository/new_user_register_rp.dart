8bc75695fd59f7228f09994272af8f85c87c8d20
lucas matheus <lucasmatheusdev@gmail.com>
2022-09-23 12:03:12 -0300

    chore add test

--- lib/app/modules/NewUser/repository/new_user_register_rp.dart
+++ lib/app/modules/NewUser/repository/new_user_register_rp.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:house_easy/app/modules/NewUser/model/new_user_model.dart';
 
 class NewUserRegisterRP {
@@ -9,7 +8,7 @@ class NewUserRegisterRP {
     try {
       _credential = await _auth.createUserWithEmailAndPassword(
           email: newUser.email!, password: newUser.password!);
-    
+
       await _updateName(newUser.name!);
     } on FirebaseAuthException catch (_) {
       return _credential;
